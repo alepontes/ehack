193756007dccbb598386979e93c0369791bbaee5
alepontes <alexandrelopespontes@gmail.com>
2018-05-12 13:22:46 -0300

    Consumindir API

--- script.js
+++ script.js
@@ -4,37 +4,40 @@ carrega();
 
 function carrega() {
 
-    const conteudo = api();
     let itens = '';
 
+    api((conteudo) => {
+        for (const item of conteudo) {
+            if (conteudo) {
+                for (const item of conteudo) {
+                    add(item);
+                }//for
+            }
+        }
+    })
 
-    console.log(conteudo)    
-
-
-    // for (const item of conteudo) {
-    //     console.log(item.titulo)
-    //     console.log(item.preco)
-    // }
-
-
-    //     if (conteudo) {
-    //         for (const item of conteudo) {
-    //             itens += `
-    //             <div class="item" onclick="">
-    //             <img src="${item.img}" alt="">
-    //             <p>${item.titulo}</p>
-    //             <p>${item.preco}</p>
-    //             </div>`
-    //         }//for
-
-    //         add(itens);
-    //     }
 }
 
 function add(item) {
-    let vitrine = document.querySelector('.vitrie');
-    vitrine = vitrine.innerHTML + item
-    resu = document.createElement('');
+
+
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    let titulo = document.createElement("p");
+    let preco = document.createElement("p");
+
+    div.className = "item";
+    img.src = item.img;
+    titulo.innerHTML = item.titulo;
+    preco.innerHTML = item.preco;
+
+    div.appendChild(img);
+    div.appendChild(titulo);
+    div.appendChild(preco);
+
+    let vitrine = document.querySelector('.vitrine');
+    vitrine.appendChild(div);
+
 }
 
 
@@ -45,7 +48,7 @@ function api(callback) {
     $.ajax({
         type: "GET",
         url: link,
-        success: data => { return data },
+        success: data => { callback(data) },
         error: data => { return data }
     });
 
